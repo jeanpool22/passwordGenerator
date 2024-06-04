57daa5422516a36fc5e4ba39d2bbff746e417cac
Jean Pool Zambrano <jean_pool.98@hotmail.com>
2024-06-04 18:24:30 -0500

    Segunda versión del generador de contraseñas

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int length;
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        String[] caracteres = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                               "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B",
-                               "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
-                               "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4",
-                               "5", "6", "7", "8", "9", "0", "#", "$", "%", "&", "*", "+", "[", "]"};
+        char[] characters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+                             'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                             'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4',
+                             '5', '6', '7', '8', '9', '0', '#', '$', '%', '&', '*', '+', '[', ']',
+                             '!'};
 
-        for (int i = 0; i < 12; i++) {
-            int numero = random.nextInt(0, caracteres.length + 1);
-            System.out.print(caracteres[numero]);
+        System.out.print("Length: ");
+        length = scanner.nextInt();
+
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(0, characters.length + 1);
+            System.out.print(characters[number]);
         }
     }
 }
